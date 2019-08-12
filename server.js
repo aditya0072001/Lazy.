04d049482920d5dd04389e8ddd47cbd03c318d61
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-08-12 08:47:37 +0530

    get added

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ var serveStatic = require('serve-static');
 const app = express();
 app.use(cookieParser())
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.get('/about', (req, res) => {
+    res.redirect('./views/About');
+})
+
+app.get('/Tv', (req, res) => {
+    res.redirect('./components/Tv');
+})
 const port = process.env.PORT || 5000;
 app.listen(port);
