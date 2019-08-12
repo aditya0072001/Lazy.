7daa7460f2a9a996cbaa508630560d155881d4af
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-08-12 07:43:15 +0530

    cookies added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser')
 var serveStatic = require('serve-static');
 const app = express();
-
+app.use(cookieParser())
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 5000;
 app.listen(port);
