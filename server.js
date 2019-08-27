3c8f15cfd5ff9ea156ff6facdc8135e3786e45ea
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-08-27 21:31:42 +0530

    fix

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var path = require('path');
 var serveStatic = require('serve-static');
 var cookieParser = require('cookie-parser')
 const app = express();
-app.use(cookieParser())
-    //app.use('/', serveStatic(path.join(__dirname, '/dist')));
-const port = process.env.PORT || 5000;
+app.use(cookieParser());
+
 app.use('/', serveStatic(path.join(__filename, 'public/index.html')));
+//app.use('/', serveStatic(path.join(__dirname, '/dist')));
+const port = process.env.PORT || 5000;
+
+
 app.listen(port);
